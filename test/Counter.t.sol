a1c1fb0aa3e91dde581a8323b9d6928911607f84
Luigi <luigi@umbrellavalidator.com>
2024-10-17 12:26:22 -0700

    Formatting with forge fmt

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -6,14 +6,20 @@ import {Counter} from "src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
-    string private svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text x='10' y='20'>Hello, World!</text></svg>";
+    string private svg =
+        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text x='10' y='20'>Hello, World!</text></svg>";
     string private svgBase64;
-    string private svgBase64v502 = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
-    string private svgBase64v510 = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
-    string private svgBase64v490 = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
-    string private svgBase64v480 = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
-    string private svgBase64v450 = "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
-    
+    string private svgBase64v502 =
+        "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
+    string private svgBase64v510 =
+        "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
+    string private svgBase64v490 =
+        "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
+    string private svgBase64v480 =
+        "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
+    string private svgBase64v450 =
+        "PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMDAgMTAwJz48dGV4dCB4PScxMCcgeT0nMjAnPkhlbGxvLCBXb3JsZCE8L3RleHQ+PC9zdmc+";
+
     function setUp() public {
         counter = new Counter();
     }
